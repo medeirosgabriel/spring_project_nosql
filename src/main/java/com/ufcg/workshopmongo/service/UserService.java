9baa9482950d8f3e75ae4433478ef6c4c8407244
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2021-04-01 14:59:13 -0300

    insert user ok

--- src/main/java/com/ufcg/workshopmongo/service/UserService.java
+++ src/main/java/com/ufcg/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ufcg.workshopmongo.domain.User;
+import com.ufcg.workshopmongo.dto.UserDTO;
 import com.ufcg.workshopmongo.repository.UserRepository;
 import com.ufcg.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insertUser(User user) {
+		return userRepository.save(user);
+	}
+	
+	public User fromDTO(UserDTO user) {
+		return new User(user.getId(), user.getName(), user.getEmail());
 	}
 }
