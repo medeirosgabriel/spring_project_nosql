a47aaa8397c4273bc62b90732e2c220e8b2f4ea7
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2021-04-06 11:21:49 -0300

    @query

--- src/main/java/com/ufcg/workshopmongo/service/PostService.java
+++ src/main/java/com/ufcg/workshopmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.ufcg.workshopmongo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,10 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
+		return this.postRepository.findByTitleContainingIgnoreCase(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
 		return this.postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
